19dc6ce52828c1158735ce8ad91acea7509d1716
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-06-07 15:56:58 -0500

    Removing custom SDK

--- prepare_authorizer_package.sh
+++ prepare_authorizer_package.sh
@@ -15,19 +15,15 @@ cp ./lambda_function.py package
 
 echo installing python pre-requisites
 pip install -q python-jose requests==2.29.0 --target package
-# temporary copy last boto3 version that supports Amazon Verified Permissions
-# this will be deleted when boto3 will support Amazon Verified Permissions
 
-echo unzip packages
-unzip -qq -o boto.zip -d package
 export BUCKET_NAME=$1
 aws cloudformation package --template avp-authorizer-cf-template.yaml \
- --s3-bucket $BUCKET_NAME --output-template-file cf-package.yaml
+    --s3-bucket $BUCKET_NAME --output-template-file cf-package.yaml
 
 echo deploying an API GW, Lambda Authorizer and sample API to region:$4
 export POLICY_STORE_ID=$2
 export IDENTITY_URL=$3
 aws cloudformation deploy --template-file cf-package.yaml \
---stack-name avp-authorizer-stack --capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides policyStoreID=$POLICY_STORE_ID IdentityTenantUrl=$IDENTITY_URL \
---region $4
+    --stack-name avp-authorizer-stack --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides policyStoreID=$POLICY_STORE_ID IdentityTenantUrl=$IDENTITY_URL \
+    --region $4
